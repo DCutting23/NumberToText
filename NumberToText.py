04cb44d37474168542641f909c16df2fa1fdf3fd
DCutting23 <daniel.cutting@live.co.uk>
2023-08-20 20:57:40 +0100

    working up to 999

--- NumberToText.py
+++ NumberToText.py
@@ -1,7 +1,7 @@
 #converting number to text form, i.e. "20" to "Twenty"
 
 d0to9 = {"0":"zero", "1":"one", "2":"two", "3":"three", "4":"four", "5":"five", "6":"six", "7":"seven", "8":"eight", "9":"nine"}
-d10to19 = {"10":"ten", "11":"eleven", "12":"twelve", "13":"thirteen", "14":"fourteen", "15":"fifteen", "16":"sixteen", "17":"seventeen", "18":"eighteen", "19":"nineteen"}
+d10to19 = {"0":"ten", "1":"eleven", "2":"twelve", "3":"thirteen", "4":"fourteen", "5":"fifteen", "6":"sixteen", "7":"seventeen", "8":"eighteen", "9":"nineteen"}
 d10s = {"2":"twenty", "3":"thirty", "4":"fourty", "5":"fifty", "6":"sixty", "7":"seventy", "8":"eighty", "9":"ninety"}
 
 if __name__ == "__main__":
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     elif (9 < number_int < 20):
         print("in range 10 to 19")
-        number_text = d10to19.get(number)
+        number_text = d10to19.get(number_array[1])
         print(number_text)
 
     elif(19 < number_int < 100):
@@ -43,7 +43,17 @@ if __name__ == "__main__":
         print("in range 100 to 999")
         if (number_int % 100 == 0):
             number_text = d0to9.get(number_array[0]) + " hundred"
-        elif (number_int % 100 <)
+        elif (number_int % 100 < 10):
+            number_text = d0to9.get(number_array[0]) + " hundred and " + d0to9.get(number_array[2])
+        elif (9 < number_int%100 < 20):
+            number_text = d0to9.get(number_array[0]) + " hundred and " + d10to19.get(number_array[2])
+        elif ((number_int%100)%10 == 0):
+            number_text = d0to9.get(number_array[0]) + " hundred and " + d10s.get(number_array[1])
+        else:
+            number_text = d0to9.get(number_array[0]) + " hundred and " + d10s.get(number_array[1]) + " " + d0to9.get(number_array[2])
+
+
+        print(number_text)
 
 
     else:
