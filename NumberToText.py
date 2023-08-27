d05c8679005d4a3a6048612e2d1c184109d810b9
DCutting23 <daniel.cutting@live.co.uk>
2023-08-27 23:46:25 +0100

    millions working, some edge cases have issues

--- NumberToText.py
+++ NumberToText.py
@@ -3,9 +3,14 @@
 d0to9 = {"0":"zero", "1":"one", "2":"two", "3":"three", "4":"four", "5":"five", "6":"six", "7":"seven", "8":"eight", "9":"nine"}
 d10to19 = {"1":"eleven", "2":"twelve", "3":"thirteen", "4":"fourteen", "5":"fifteen", "6":"sixteen", "7":"seventeen", "8":"eighteen", "9":"nineteen"}
 d10s = { "1":"ten", "2":"twenty", "3":"thirty", "4":"fourty", "5":"fifty", "6":"sixty", "7":"seventy", "8":"eighty", "9":"ninety"}
-noPlaces = {}
 
-def slz(digits):
+def numint(digits): #int from list of digits
+    num_str = ""
+    for digit in digits:
+        num_str += digit
+    return int(num_str)
+
+def slz(digits): #strips leading 0s
     i = 0
     while(i < len(digits)):
         if (digits[i] == "0"):
@@ -15,6 +20,7 @@ def slz(digits):
             break
         i += 1
     return digits
+
 def last_digit(digits):
     #takes single digit, returns 0 to 9 value
     return d0to9[digits[0]]
@@ -22,10 +28,8 @@ def last_digit(digits):
 def tens_and_teens(digits):
     #takes two digits in array
     #determines if last two digits are between 11 and 19, returns value in teens or 10s value if not
-    num_str = ""
-    for digit in digits:
-        num_str += digit
-    num = int(num_str)
+    num = numint(digits)
+
     if (num < 10):
         print("passing down")
         return last_digit(slz(digits))
@@ -40,10 +44,8 @@ def tens_and_teens(digits):
 def hundreds(digits):
     #takes 3 digits in array
     #gives value from hundreds place downwards
-    num_str = ""
-    for digit in digits:
-        num_str += digit
-    num = int(num_str)
+    num = numint(digits)
+
     if (num < 100):
         print("passing down")
         return tens_and_teens(slz(digits))
@@ -56,33 +58,50 @@ def hundreds(digits):
 def thousands(digits):
     #takes 4 to 6 digits in array
     #gives value from thousands place downwards
-    thousands_digits = []
-    thousands_str = ""
-    for i in range(len(digits)-3):
-        thousands_digits.append(digits[i])
-    for digit in thousands_digits:
-        thousands_str += digit
-    thousands_num = int(thousands_str)
-    
-    hundreds_digits = []
-    hundreds_str = ""
-    for i in range(len(digits)-3, len(digits)):
-        hundreds_digits.append(digits[i])
-    for digit in hundreds_digits:
-        hundreds_str += digit
-    hundreds_num = int(hundreds_str)
+    num = numint(digits)
 
-    if (hundreds_num == 0):
-        return hundreds(thousands_digits) + " thousand"
+    if (num < 1000):
+        print("passing down")
+        return hundreds(slz(digits))
     else:
-        return hundreds(thousands_digits) + " thousand " + hundreds(hundreds_digits)
+        thousands_digits = []
+        thousands_str = ""
+        for i in range(len(digits)-3):
+            thousands_digits.append(digits[i])
+        thousands_num = numint(thousands_digits)
+        
+        hundreds_digits = []
+        hundreds_str = ""
+        for i in range(len(digits)-3, len(digits)):
+            hundreds_digits.append(digits[i])
+        hundreds_num = numint(hundreds_digits)
 
-    
+        if (hundreds_num == 0):
+            return hundreds(thousands_digits) + " thousand"
+        else:
+            return hundreds(thousands_digits) + " thousand " + hundreds(hundreds_digits)
 
-"""
-def millions():
+def millions(digits):
     #gives value from millions place downwards
-"""
+    num = numint(digits)
+    if (num < 1000000):
+        print("passing down")
+        return thousands(slz(digits))
+    else:
+        millions_digits = []
+        for i in range(len(digits)-6):
+            millions_digits.append(digits[i])
+        millions_num = numint(millions_digits[i])
+
+        thousands_digits = []
+        for i in range(len(digits)-6, len(digits)):
+            thousands_digits.append(digits[i])
+        thousands_num = numint(thousands_digits)
+
+        if (thousands_num == 0):
+            return thousands(millions_digits) + " million"
+        else:
+            return thousands(millions_digits) + " million " + thousands(thousands_digits)
 
 if __name__ == "__main__":
     print ("program start")
@@ -100,15 +119,7 @@ if __name__ == "__main__":
     for char in number:
         number_array.append(char)
 
-    #removes leading 0s
-    i = 0
-    while(i < len(number_array)):
-        if (number_array[i] == "0"):
-            number_array.pop(i)
-            i-=1
-        else:
-            break
-        i += 1
+    number_array = slz(number_array)
 
     if (len(number_array) < 2): #checks for number in 0 to 9 range
         print(last_digit(number_array[0]))
@@ -118,5 +129,7 @@ if __name__ == "__main__":
         print(hundreds(number_array))
     elif(len(number_array) < 7): #checks for number in 1000 to 999 999 range
         print(thousands(number_array))
+    elif(len(number_array) < 10): #checks for number in 1 000 000 to 999 999 999 range
+        print(millions(number_array))
     else:
         print("hit else")
